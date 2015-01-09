1d5d6a48160128cca8f724d789f075a0f23d635a
Harvey Sanders <sanders.harvey@gmail.com>
2015-01-08 22:00:11 -0600

    add listToArray function

--- chapter4/list.js
+++ chapter4/list.js
@@ -11,13 +11,16 @@ var arrayToList = function(array) {
 };
 
 var listToArray = function(list) {
-
+	var array = [];
+	for (var node = list; node; node = node.rest) {
+		array.push(node.value);
+	}return array;
 };
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-//console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
