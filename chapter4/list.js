38b4109a0b817b2773910f9d57c147bec4a34f61
Harvey Sanders <sanders.harvey@gmail.com>
2015-01-08 23:04:40 -0600

    update

--- chapter4/list.js
+++ chapter4/list.js
@@ -12,10 +12,29 @@ var arrayToList = function(array) {
 
 var listToArray = function(list) {
 	var array = [];
-	for (var node = list; node; node = node.rest) {
+	for (var node = list; node; node = node.rest) { //since node.rest will eventually return null, the for loop will stop.
 		array.push(node.value);
 	}return array;
 };
+
+// creates a new list that adds the element to the front of the input list
+var prepend = function(element, list) {
+	var newList = {value: element, rest: list};
+	return newList;
+};
+
+// takes a list and a number and returns the element at the given position 
+// in the list, or undefined when there is no such element.
+var nth = function(list, number) {
+	//return listToArray(list)[number];
+	if (!list) 
+		return undefined;
+	else if(n === 0)
+		return list.value;
+	else
+		return nth(list.rest, n-1);
+};
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
